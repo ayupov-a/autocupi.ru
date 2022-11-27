4a7df843987caaf73f227f90aaf377d1616fc019
ayupov <aigiz.ayupov1917@outlook.com>
2022-11-27 23:34:55 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AUTOCUPI - Сервис по подбору автомобилей</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <p>hello,world!</p>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container">
+
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="container">
+
+        </div>
+    </footer>
+
+
+</body>
+</html>
